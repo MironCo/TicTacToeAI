697a9a20380e78afc0f3642c1a4982ae689b883c
MironCo <64372536+MironCo@users.noreply.github.com>
2022-04-10 15:41:21 -0400

    The AI is going AAAAAAAAAAHHHH
    Please help I don't know why its so slow. I guess its time to draw it out \:
    It really should be just 9 factorial

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream> 
+#include <vector>
 
 struct BoardPosition
 {
     char board[3][3];
+    
+    void Reset()
+    {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                board[x][y] = (char)((x*3) + y + '1');
+            }
+        }
+    }  
 };
 
 class Board
@@ -10,17 +20,9 @@ class Board
 public:
     BoardPosition board;
 public:
-    Board() = default;
-    
-    void Reset()
-    {
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
-                board.board[x][y] = (char)((x*3) + y + '1');
-            }
-        }
-    }   
-
+    Board() {
+        board.Reset();
+    }
     void Display() {    
         std::cout << "\n-----------\n";
         for (int x = 0; x < 3; x++) {
@@ -37,40 +39,85 @@ public:
 struct Node
 {
     BoardPosition data;
-    BoardPosition* children;
+    std::vector<Node*> children;
 
     int line;
 
     Node() = default;
 
-    Node(int _line, BoardPosition &_value) {
+    Node(int _line, BoardPosition &_data) {
         line = _line;
-        data = _value;
-        children = (BoardPosition*)malloc((9 - line) * sizeof(BoardPosition)); 
+        data = _data;
+        //GenerateNodes();
     }
-    void GenerateNode() {
-        
+    void GenerateNodes() {
+        char turn;
+        if ((line+1) % 2 == 1) turn = 'X';
+        else turn ='O';
+
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                Node* newNode = new Node(line+1, data);
+                
+                if (newNode->data.board[x][y] != 'X' && newNode->data.board[x][y] != 'O')
+                {
+                    newNode->data.board[x][y] = turn;
+                    children.push_back(newNode);
+                } else delete(newNode);
+            }
+        }
+
+        if (line < 9) for (Node* &node : children) node->GenerateNodes();
+    }
+
+    void DisplayTree()
+    {
+        /*std::cout << "\nLine: " << line << std::endl;
+        std::cout << "-----------\n";
+        for (int x = 0; x < 3; x++) {
+            
+            for (int y = 0; y < 3; y++) {
+                std::cout << " " << data.board[x][y] << " ";
+                if (y != 2) std::cout << "|";
+            }
+            std::cout << "\n-----------\n";
+        }*/
+        std::cout << "Line " << line << ": " << "Size " << children.size();
+        for(Node* &node : children) node->DisplayTree();
     }
 };
 
 class AI
 {
 public:
-    Node rootNodes[9];
+    std::vector<Node*> nodes;
 public:
     AI() {
         
     }
     void Generate() {
-        
+        int x = 0, y = 0;
+        //for (int x = 0; x < 1; x++) {
+        //    for (int y = 0; y < 1; y++) {
+                BoardPosition newPos;
+                newPos.Reset();
+                newPos.board[x][y] = 'X';
+
+                int line = 1;
+                Node* node = new Node(line, newPos);
+                nodes.push_back(node);
+                for (Node* &node : nodes) node->GenerateNodes();
+        //    }
+        //}
     }
-    static inline void DisplayBoard(BoardPosition &board)
+    static inline void DisplayBoard(Node &board)
     {
-        std::cout << "\n-----------\n";
+        std::cout << "\nLine: " << board.line << std::endl;
+        std::cout << "-----------\n";
         for (int x = 0; x < 3; x++) {
             
             for (int y = 0; y < 3; y++) {
-                std::cout << " " << board.board[x][y] << " ";
+                std::cout << " " << board.data.board[x][y] << " ";
                 if (y != 2) std::cout << "|";
             }
             std::cout << "\n-----------\n";
@@ -81,7 +128,11 @@ public:
 int main()
 {
     Board board;
-    board.Reset();
+
+    AI ai;
+    ai.Generate();
+
+    ai.nodes[0]->DisplayTree();
 
     return 0;
 }
