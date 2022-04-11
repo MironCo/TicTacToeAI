870c17a5bfab6d270c7645fb68433c7bac08b202
MironCo <64372536+MironCo@users.noreply.github.com>
2022-04-11 14:51:03 -0400

    Centralized Generation

--- main.cpp
+++ main.cpp
@@ -4,15 +4,7 @@
 struct BoardPosition
 {
     char board[3][3];
-    
-    void Reset()
-    {
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
-                board[x][y] = (char)((x*3) + y + '1');
-            }
-        }
-    }  
+    signed char value;
 };
 
 class Board
@@ -21,7 +13,7 @@ public:
     BoardPosition board;
 public:
     Board() {
-        board.Reset();
+        
     }
     void Display() {    
         std::cout << "\n-----------\n";
@@ -48,68 +40,115 @@ struct Node
     Node(int _line, BoardPosition &_data) {
         line = _line;
         data = _data;
-        //GenerateNodes();
-    }
-    void GenerateNodes() {
-        char turn;
-        if ((line+1) % 2 == 1) turn = 'X';
-        else turn ='O';
-
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
-                Node* newNode = new Node(line+1, data);
-                
-                if (newNode->data.board[x][y] != 'X' && newNode->data.board[x][y] != 'O')
-                {
-                    newNode->data.board[x][y] = turn;
-                    children.push_back(newNode);
-                } else delete(newNode);
-            }
-        }
-
-        if (line < 9) for (Node* &node : children) node->GenerateNodes();
-    }
-
-    void DisplayTree()
-    {
-        /*std::cout << "\nLine: " << line << std::endl;
-        std::cout << "-----------\n";
-        for (int x = 0; x < 3; x++) {
-            
-            for (int y = 0; y < 3; y++) {
-                std::cout << " " << data.board[x][y] << " ";
-                if (y != 2) std::cout << "|";
-            }
-            std::cout << "\n-----------\n";
-        }*/
-        std::cout << "Line " << line << ": " << "Size " << children.size();
-        for(Node* &node : children) node->DisplayTree();
     }
 };
 
 class AI
 {
+private:
+    const signed char LOSE = -10;
+    const signed char WIN = 10;
 public:
     std::vector<Node*> nodes;
 public:
     AI() {
         
     }
+
+    void ResetBoard(BoardPosition &board){
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                board.board[x][y] = (char)((x*3) + y + '1');
+            }
+        }
+    } 
+    
+    bool CheckForWin(BoardPosition &board, char turn) {
+        bool won = false;
+        
+        //check horizontal
+        if (board.board[0][0] == turn && board.board[1][0] == turn && board.board[2][0] == turn) won = true;
+        else if (board.board[0][1] == turn && board.board[1][1] == turn && board.board[2][1] == turn) won = true;
+        else if (board.board[0][2] == turn && board.board[1][2] == turn && board.board[2][2] == turn) won = true;
+        
+        //check vertical
+        else if (board.board[0][0] == turn && board.board[1][0] == turn && board.board[2][0] == turn) won = true;
+        else if (board.board[0][1] == turn && board.board[1][1] == turn && board.board[2][1] == turn) won = true;
+        else if (board.board[0][2] == turn && board.board[1][2] == turn && board.board[2][2] == turn) won = true;
+
+        //diagonally
+        else if (board.board[0][0] == turn && board.board[1][1] == turn && board.board[2][2] == turn) won = true;
+        else if (board.board[0][2] == turn && board.board[1][1] == turn && board.board[2][0] == turn) won = true;
+
+        if (won) std::cout << "Won Position" << std::endl;
+        return won;
+    }
+
+    void Evaluate(BoardPosition &board, int line) {
+        char turn;
+        if (line % 2 == 1) turn = 'X';
+        else turn ='O';
+
+       if (CheckForWin(board, turn)) board.value = WIN;
+    }
+
     void Generate() {
-        int x = 0, y = 0;
-        //for (int x = 0; x < 1; x++) {
-        //    for (int y = 0; y < 1; y++) {
+        for (int x = 0; x < 1; x++) {
+            for (int y = 0; y < 1; y++) {
                 BoardPosition newPos;
-                newPos.Reset();
+                ResetBoard(newPos);
                 newPos.board[x][y] = 'X';
 
                 int line = 1;
+                Evaluate(newPos, line);
                 Node* node = new Node(line, newPos);
                 nodes.push_back(node);
-                for (Node* &node : nodes) node->GenerateNodes();
-        //    }
-        //}
+                for (Node* &node : nodes) GenerateNodes(node);
+            }
+        }
     }
+
+     void GenerateNodes(Node* currentNode) {
+        char turn;
+        if ((currentNode->line+1) % 2 == 1) turn = 'X';
+        else turn ='O';
+
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                Node* newNode = new Node(currentNode->line+1, currentNode->data);
+                
+                if (newNode->data.board[x][y] != 'X' && newNode->data.board[x][y] != 'O')
+                {
+                    newNode->data.board[x][y] = turn;
+                    Evaluate(newNode->data, currentNode->line+1);
+                    currentNode->children.push_back(newNode);
+                } else delete(newNode);
+            }
+        }
+
+        if (currentNode->line < 9) 
+            for (Node* &node : currentNode->children) {
+                if (node->data.value != LOSE && node->data.value != WIN) GenerateNodes(node);
+            }
+    }
+
+    void DisplayTree(Node* currentNode)
+    {
+        if (currentNode->line == 9)
+        {
+            std::cout << "\nLine: " << currentNode->line << std::endl;
+            std::cout << "-----------\n";
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    std::cout << " " << currentNode->data.board[x][y] << " ";
+                    if (y != 2) std::cout << "|";
+                }
+                std::cout << "\n-----------\n";
+            }
+        }
+        for (Node* &node : currentNode->children) DisplayTree(node);
+    }
+
     static inline void DisplayBoard(Node &board)
     {
         std::cout << "\nLine: " << board.line << std::endl;
@@ -127,12 +166,9 @@ public:
 
 int main()
 {
-    Board board;
-
     AI ai;
     ai.Generate();
-
-    ai.nodes[0]->DisplayTree();
+    ai.DisplayTree(ai.nodes[0]);
 
     return 0;
 }
